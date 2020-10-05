6876179a9dc7fd3f53014d641a18e759f68f1169
Jade Pennig <jpennig@salesforce.com>
2020-10-05 16:50:35 -0500

    Fix to register web components in Storybook window instead of in class files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,7 @@
+import SDSButton from '../sds/Button/button'
+import C360Welcome from '../intro/Welcome/welcome'
+
 import './globals.css'
+
+customElements.define('sds-button', SDSButton)
+customElements.define('c360-welcome', C360Welcome)
