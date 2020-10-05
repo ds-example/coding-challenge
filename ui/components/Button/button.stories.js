07d6b09baa440440e957e608c62af23e1ef5542e
Jade Pennig <jpennig@salesforce.com>
2020-10-05 14:16:20 -0500

    Prettierrc and formatting

--- ui/components/Button/button.stories.js
+++ ui/components/Button/button.stories.js
@@ -2,19 +2,19 @@ import Button from 'elix/define/Button'
 import './button.css'
 
 export default {
-    title: 'Components/Button'
+  title: 'SDS/Button'
 }
 
 export const BasicHTML = () => {
-    const btn = document.createElement('button');
-    btn.type = 'button';
-    btn.innerText = 'Trigger Action';
-    btn.addEventListener('click', () => action('This was clicked')());
-    return btn;
+  const btn = document.createElement('button')
+  btn.type = 'button'
+  btn.innerText = 'Trigger Action'
+  btn.addEventListener('click', () => action('This was clicked')())
+  return btn
 }
 
 export const ElixButton = () => {
-    const button = new Button()
-    button.innerText = 'This is a test'
-    return button
+  const button = new Button()
+  button.innerText = 'This is a test'
+  return button
 }
