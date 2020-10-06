ab6be205cfb3c7831755a599d81fb2baf0750d81
Jade Pennig <jpennig@salesforce.com>
2020-10-06 15:32:16 -0500

    Remove WIP code

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,10 @@
 import SDSButton from '../src/sds/Button/button'
-import SDSImage from '../src/sds/Image/image'
 import C360Welcome from '../src/intro/Welcome/welcome'
 
 import './globals.css'
 
 // Define SDS Web Components
 customElements.define('sds-button', SDSButton)
-customElements.define('sds-image', SDSImage)
 
 // Define C360 Web Components
 customElements.define('c360-welcome', C360Welcome)
