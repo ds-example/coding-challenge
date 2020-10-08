aff147aa53e8827b108154e5e9b14380bfe80caf
Jade Pennig <jpennig@salesforce.com>
2020-10-08 09:18:46 -0500

    Add C360 Card component

---
+++ src/c360/Card/card.js
@@ -0,0 +1,32 @@
+/**
+ * This Class is built on top of Elix's "ReactiveElement"
+ * base element. Think of this like React's Component class.
+ *
+ * Documentation on ReactiveElement can be found here:
+ * https://component.kitchen/elix/ReactiveElement
+ *
+ * Also check out documentation on ReactiveMixin, to better
+ * understand how component state is managed:
+ * https://component.kitchen/elix/ReactiveMixin
+ * (you can also see `src/sds/Button/button.js` for a state management example)
+ */
+
+import ReactiveElement from 'elix/src/core/ReactiveElement'
+import { template } from 'elix/src/base/internal'
+import { templateFrom } from 'elix/src/core/htmlLiterals.js'
+
+import stylesheet from './card.css'
+
+class C360Card extends ReactiveElement {
+  get [template]() {
+    return templateFrom.html`
+      <style>
+        ${stylesheet}
+      </style>
+
+      <p>[Your Card Component Template Goes Here]</p>
+    `
+  }
+}
+
+export default C360Card
