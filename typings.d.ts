55c5f597880a1de365f3f977099b31c92ec41dc0
Salem Cobalt <salem@knapsack.cloud>
2022-02-06 23:32:51 -0500

    feat: update Prettier config + wire up basic typescript config

---
+++ typings.d.ts
@@ -0,0 +1,19 @@
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.mdx' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.md' {
+  const content: Record<string, string>;
+  export default content;
+}
